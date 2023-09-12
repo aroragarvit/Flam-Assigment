334aafb4c886bf75b359608d1f12b274d42294c1
Garvit <agarvit1142000@gmail.com>
2023-09-12 19:10:26 +0530

    updated Request, Response in UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,15 +1,18 @@
-import UserRepository from "../database/UserRepository";
-import User from "../models/User";
+import UserRepository from '../database/UserRepository';
+import User from '../models/User';
+import { Request, Response } from 'express';
+
 class UserController {
-    public static async addUser(req: Request, res: Response): Promise<void> {
-        try{
-            const{username:string, email:string, password:string} = req.body;
-            const user = User.getInstance(username, email, password);
-            const userRepository = new UserRepository();
-            await userRepository.addUser(user);
-            res.status(200).send('User added successfully');
-        }catch(err){
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
+  public static async addUser(req: Request, res: Response): Promise<void> {
+    try {
+      const { username, email, password } = req.body;
+      const user = User.getInstance(username, email, password);
+      const userRepository = new UserRepository();
+      await userRepository.addUser(user);
+      res.status(200).send('User added successfully');
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+  }
 }
